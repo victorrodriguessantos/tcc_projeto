0a2a7ad6b9f44d84d2f07c47dac65744222893bc
Victor <victorrodrigues288@gmail.com>
2024-07-26 17:40:33 -0300

    Atualização 1.0

--- public/src/index.js
+++ public/src/index.js
@@ -168,7 +168,7 @@ async function listarClientes() {
                     <span><strong>CPF/CNPJ:</strong> ${cliente.cpf_cnpj}</span>
                 </div>
                 <div class="client-actions">
-                    <button onclick='visualizarCliente(${JSON.stringify(cliente)})'><i class=""></i>Visualizar</button>
+                    <button class="visualizar" onclick='visualizarCliente(${JSON.stringify(cliente)})'><i class=""><i class="fi-ss-overview"></i></button>
                     <button class="editar" onclick='abrirEditarCliente(${JSON.stringify(cliente)})'><i class="fi fi-ss-file-edit"></i></button>
                     <button class="excluir" onclick='excluirCliente(${cliente.id_cliente})'><i class="fi-ss-trash"></i></button>
                 </div>
